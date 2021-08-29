8f2268e4a9ffb563ba928ed1a429edcbb959bd9b
daonancy <1726388663@qq.com>
2021-08-29 11:15:34 +0800

    第一次提交代码

---
+++ TestNG_01/src/test/java/org/example/AppTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+
+    @Test
+    public void runOtherTest1() {
+        System.out.println("@Test - runOtherTest1");
+    }
+
+    @Test
+    public void runOtherTest2() {
+        System.out.println("@Test - runOtherTest2");
+    }
+
+
+
+}
---
+++ TestNG_01/src/test/java/org/example/TestDBConnection.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class TestDBConnection {
+    @Test
+    public void runOtherTest3() {
+        System.out.println("@Test - runOtherTest3");
+    }
+
+    @Test
+    public void runOtherTest4() {
+        System.out.println("@Test - runOtherTest4");
+    }
+
+}
